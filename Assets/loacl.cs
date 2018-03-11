6ac298da5a7390d53e501086cfbadba654e566e1
user-PC\user <wei520410@kimo.com>
2018-03-11 20:53:27 +0800

    test04

--- Assets/loacl.cs
+++ Assets/loacl.cs
@@ -7,6 +7,7 @@ public class loacl : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print ("Hello world!");
+		print ("Mother Fucker!");
 	}
 	
 	// Update is called once per frame
