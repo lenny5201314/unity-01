4f7861e9780fc907d78f574df92222ed2968ed76
THESTORMOFWEIHS\Administrator <smile_8724@yahoo.com.tw>
2018-03-11 21:03:28 +0800

    COUNTINGGGGGG

--- Assets/loacl.cs
+++ Assets/loacl.cs
@@ -7,6 +7,7 @@ public class loacl : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print ("Hello world!");
+		print ("NVM I GIVE UP .");
 	}
 	
 	// Update is called once per frame
